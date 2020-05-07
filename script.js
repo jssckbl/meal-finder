06ab15401a297b323053d867f7abb081152c5ac8
jssckbl <jessicawoudsma@gmail.com>
2020-05-07 17:43:03 -0500

    set up initial variables in JS, submit button triggers searchMeal function, alert appears if input is empty when search button is clicked

---
+++ script.js
@@ -0,0 +1,27 @@
+const search = document.getElementById("search"),
+  submit = document.getElementById("submit"),
+  random = document.getElementById("random"),
+  mealsEl = document.getElementById("meals"),
+  resultHeading = document.getElementById("result-heading"),
+  single_mealEl = document.getElementById("single-meal");
+
+// Search meal and fetch from API
+// since this is a submit, we need to prevent the default behavior by adding a preventDefault event so it doesn't actually submit to a file
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get the search term
+  const term = search.value;
+
+  // Check for empty --- 'trim' removes excess white space
+  if (term.trim()) {
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+// Event listeners
+submit.addEventListener("submit", searchMeal);
